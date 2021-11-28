08602744f193c0d8858777edfd76724a8858b84c
Ronan Avila <avila.ronan@gmail.com>
2021-11-27 22:09:25 -0300

    autenticacao finalizada

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, duplicate_ignore
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:prova/create_account.dart';
 import 'package:prova/menu_page.dart';
 import 'package:prova/sobre_page.dart';
 
@@ -12,8 +14,8 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-String usuario = '';
-String senha = '';
+var txtUser = TextEditingController();
+var txtPassword = TextEditingController();
 
 class _LoginPageState extends State<LoginPage> {
   @override
@@ -32,9 +34,7 @@ class _LoginPageState extends State<LoginPage> {
                   Image.asset('assets/images/Icon-192.png'),
                   // ignore: prefer_const_constructors
                   TextField(
-                    onChanged: (text) {
-                      usuario = text;
-                    },
+                    controller: txtUser,
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Usuário',
@@ -47,9 +47,7 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   // ignore: prefer_const_constructors
                   TextField(
-                    onChanged: (text) {
-                      senha = text;
-                    },
+                    controller: txtPassword,
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Senha',
@@ -62,35 +60,21 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   ElevatedButton(
                       onPressed: () {
-                        if (usuario == 'ronan' && senha == '123') {
-                          Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                  builder: (context) => MenuPage()));
-                        } else {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: Text('Erro no Login'),
-                              content: Text('Usuário ou senha incorreto'),
-                              actions: [
-                                ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pushReplacement(
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  LoginPage()));
-                                    },
-                                    child: Text('Voltar'))
-                              ],
-                            ),
-                          );
-                        }
+                        login(txtUser.text, txtPassword.text);
                       },
                       child: Text('Entrar')),
                   SizedBox(
                     height: 15,
                   ),
-
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => CreateAccountPage()));
+                      },
+                      child: Text('Criar Conta')),
+                  SizedBox(
+                    height: 15,
+                  ),
                   IconButton(
                     icon: const Icon(Icons.info),
                     color: Colors.blue,
@@ -104,5 +88,27 @@ class _LoginPageState extends State<LoginPage> {
             )),
       ),
     );
+  }
+
+  void login(email, password) {
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((value) {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => MenuPage()));
+    }).catchError((erro) {
+      var mensagem = '';
+      if (erro.code == 'iser-not-found') {
+        mensagem = 'ERRO: Usuário não encontrado';
+      } else if (erro.code == 'wrong-password') {
+        mensagem = 'ERRO: ERRO: Usuário ou Senha incorreta';
+      } else if (erro.code == 'invalid-email') {
+        mensagem = 'ERRO: Usuário ou Senha incorreta';
+      } else {
+        mensagem = erro.message;
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(mensagem), duration: Duration(seconds: 2)));
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:prova/login_page.dart';
+import 'login_page.dart';
+import 'create_account.dart';
 //import 'package:prova/menu_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/create_account': (context) => const CreateAccountPage(),
+      },
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginPage(),
-    );
-  }
-}
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         // is not restarted.
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const LoginPage(),
+//     );
+//   }
+// }
